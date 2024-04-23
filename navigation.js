33b054e028d4362e27ad4eb13fc863b032327b26
Marietta Oravainen <128025570+mariettaoravainen@users.noreply.github.com>
2024-04-23 15:06:25 +0300

    Add files via upload

--- navigation.js
+++ navigation.js
@@ -13,6 +13,7 @@ import FieldsScreenModify from './screens/settingsScreens/fieldsModify';
 import TitlesScreenModify from './screens/settingsScreens/titlesModify';
 import modAccount from './screens/modAccount';
 import ProfileModify from './screens/settingsScreens/profileModify';
+import Passcode from './screens/settingsScreens/passcode';
 
 
 const Stack = createNativeStackNavigator();
@@ -21,11 +22,12 @@ const Navigation = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="ModifyProfile" component={modAccount} />
+        <Stack.Screen name="Settings" component={Settings} />
+        <Stack.Screen name="ModifyAccount" component={modAccount} />
+        <Stack.Screen name="Passcode" component={Passcode} />
         <Stack.Screen name="FModify" component={FieldsScreenModify} />
         <Stack.Screen name="TModify" component={TitlesScreenModify} />
         <Stack.Screen name="ProfileModify" component={ProfileModify} />
-        <Stack.Screen name="Settings" component={Settings} />
         <Stack.Screen name="HowItWorks" component={HowItWorks} />
         <Stack.Screen name="Home" component={HomeScreen} />
         <Stack.Screen name="Login" component={LoginScreen} />
